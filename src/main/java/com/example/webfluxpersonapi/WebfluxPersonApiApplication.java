4f9b0736b9b042c83cad5b08706a8a5738ddcb5e
samitkumarpatel-work <SamitKumar.Patel@maersk.com>
2023-08-24 20:12:06 +0200

    anyOf request mapping achived

--- src/main/java/com/example/webfluxpersonapi/WebfluxPersonApiApplication.java
+++ src/main/java/com/example/webfluxpersonapi/WebfluxPersonApiApplication.java
@@ -1,5 +1,10 @@
 package com.example.webfluxpersonapi;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -19,7 +24,6 @@ public class WebfluxPersonApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WebfluxPersonApiApplication.class, args);
 	}
-
 	@Bean
 	RouterFunction routerFunction() {
 		return RouterFunctions
@@ -32,6 +36,7 @@ public class WebfluxPersonApiApplication {
 						.DELETE("{id}", request -> ServerResponse.noContent().build())
 				)
 				.build();
+
 	}
 
 	private Mono<ServerResponse> persistPerson(ServerRequest request) {
@@ -45,20 +50,47 @@ public class WebfluxPersonApiApplication {
 	}
 
 	private Mono<Person> dbPersist(Person person) {
+		ObjectMapper mapper = new ObjectMapper();
 		person
-				.contacts()
+				.getContacts()
 				.stream()
-				.forEach(p -> log.info("contacts - {}", p.data()));
+				.forEach(p -> {
+					log.info("contacts {}", p);
+				});
 
-		return Mono.fromCallable(() -> person);
+		return Mono.fromCallable(() -> person)	;
 	}
-
 }
 
-enum ContactType{
+enum ContactType {
 	ADDRESS, PHONE
 }
-record Phone(int number, ContactType type){}
-record Address(String address1, String address2, ContactType type){}
-record Contacts<T extends Address, Phone>(){}
-record Person(int id, String name, int age, List<Contacts> contacts){}
+
+//record Phone(Integer number,ContactType type) {}
+//record Address (String address1, String address2, ContactType type) {}
+//record Contact<T>(T data) {}
+
+abstract class Contact {
+	ContactType contactType;
+}
+
+@Setter @Getter @AllArgsConstructor @NoArgsConstructor
+class Phone extends Contact {
+	String number;
+}
+@Setter @Getter @AllArgsConstructor @NoArgsConstructor
+class Address extends Contact {
+	String address1;
+	String address2;
+}
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+class Person {
+	int id;
+	String name;
+	int age;
+	List<? super Contact> contacts;
+}
